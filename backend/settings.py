f4177b2c7b35319589a3a55cb2d2009846ee61a7
Rishi Tripathy <rishiraj.tripathy.33@gmail.com>
2020-01-14 23:30:21 -0500

    fixed issue w static files

--- backend/settings.py
+++ backend/settings.py
@@ -131,4 +131,5 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
